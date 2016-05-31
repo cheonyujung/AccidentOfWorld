7eced17fda6be273e0bd4bb85ba1938b58d6a15f
Sun Yeong <vmffkxlgnqh1@gmail.com>
2016-05-31 17:14:31 +0900

    구글 맵 프래그먼트 변환중 실패.. 다 주석처리 했으니 딱히 건드릴 필요 없음

---
+++ app/src/main/java/com/example/cheonyujung/accidentofworld/fragment/mapActivity.java
@@ -0,0 +1,20 @@
+//package com.example.cheonyujung.accidentofworld.fragment;
+//
+//import android.app.FragmentManager;
+//import android.os.Bundle;
+//
+//import com.example.cheonyujung.accidentofworld.R;
+//
+///**
+// * Created by vmffkxlgnqh1 on 2016. 5. 28..
+// */
+//public class mapActivity extends singleFragmentActivity{
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        FragmentManager fm = getFragmentManager();
+//        fm.beginTransaction()
+//                .replace(R.id.body, com.example.cheonyujung.accidentofworld.fragment.WorldMapFragment.getInstence())
+//                .commit();
+//    }
+//}
